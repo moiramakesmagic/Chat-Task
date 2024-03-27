ffdb5daf74fb5a9f0038a59b494648cc3d81ed3b
moiramakesmagic <165111394+moiramakesmagic@users.noreply.github.com>
2024-03-26 22:10:15 -0400

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,10 +1,10 @@
 const auth = (function () {
 	// Authentication and channels - required
-	const channel = ""; // your channel
-	const username = ""; // bot account or your channel
+	const channel = "mavkamakesmagic"; // your channel
+	const username = "eoryk"; // bot account or your channel
 
 	// You may or may not include the "oauth:" portion of your token
-	const oauth = "";
+	const oauth = "gnz45gxiev72cgs99ksl34a1r87tzv";
 
 	return {
 		channel,
